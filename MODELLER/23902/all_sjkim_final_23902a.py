8abf4839e8f7df4f5d22c47a039db20d1f948678
Ben Webb <ben@salilab.org>
2018-03-28 11:20:28 -0700

    Add missing Modeller inputs to generate model 23902

---
+++ MODELLER/23902/all_sjkim_final_23902a.py
@@ -0,0 +1,21 @@
+from modeller import *
+from modeller.automodel import *
+
+log.verbose()
+env = environ()
+env.io.atom_files_directory = ['../templates']
+
+class MyModel(automodel):
+    def special_patches(self, aln):
+        self.rename_segments('A', 0)
+
+a = MyModel(env, 
+            alnfile='all_align_final_23902a.ali',
+            knowns=('15133B', '15133A', '2jo8A', '2qiwA', '3cigH', '2eloA',
+                    '3guzA', '3i4rB'),
+            sequence='23902a',
+            assess_methods=(assess.DOPE, assess.GA341))
+a.starting_model = 1
+a.ending_model = 200
+
+a.make()
