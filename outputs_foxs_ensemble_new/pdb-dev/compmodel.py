586d605176683b256824f68273ac72b5f43268a7
Ben Webb <ben@salilab.org>
2018-03-28 14:10:48 -0700

    Add coordinates of starting model

--- outputs_foxs_ensemble_new/pdb-dev/compmodel.py
+++ outputs_foxs_ensemble_new/pdb-dev/compmodel.py
@@ -1,17 +1,38 @@
 import ihm.startmodel
 import ihm.metadata
+import ihm.model
+import Bio.PDB
 
 pdb_file = '../../MODELLER/23904/23904.B99990030.pdb'
 offset = 1  # numbering starts at 0, but ours does at 1
 chain = 'A'
 
 class StartingModel(ihm.startmodel.StartingModel):
-    pass
+    def __init__(self, file_name, **kwargs):
+        super(StartingModel, self).__init__(**kwargs)
+        self.file_name = file_name
+
+    def get_atoms(self):
+        p = Bio.PDB.PDBParser()
+        s = p.get_structure('rep', self.file_name)
+        for model in s:
+            for nchain, chain in enumerate(model):
+                assert(nchain == 0) # should only be one chain
+                for nres, residue in enumerate(chain):
+                    for atom in residue:
+                        coord = atom.get_vector()
+                        yield ihm.model.Atom(asym_unit=self.asym_unit,
+                                seq_id=nres+1,
+                                atom_id=atom.get_id(), x=coord[0], y=coord[1],
+                                z=coord[2], type_symbol=atom.element,
+                                biso=atom.get_bfactor())
+
 
 def get_starting_modeller_model(asym):
     """Read the MODELLER model that we used as a starting guess for
        AllosMod & MES, and return it as an ihm.startmodel object."""
     p = ihm.metadata.PDBParser()
     m = p.parse_file(pdb_file)
-    return StartingModel(asym, dataset=m['dataset'], asym_id=chain,
+    return StartingModel(file_name=pdb_file, asym_unit=asym,
+                         dataset=m['dataset'], asym_id=chain,
                          templates=m['templates'], offset=offset)
